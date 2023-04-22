49c70a8d761c093352e08100377d2ad8ac6d0cb2
Taro <angyoutaro@gmail.com>
2023-04-22 11:59:49 +0700

    changing database initial

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,22 @@ const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 AppDataSource.initialize()
   .then(() => {
     console.log(`Data Source has been initialized`);
-
-    const app = express();
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-
-    app.get("/", (req: Request, res: Response) => {
-      res.json({ message: "ok" });
-    });
-
-    app.use("/user", userRouter);
-
-    app.listen(port, "0.0.0.0", () => {
-      console.log(`Example app listening at http://localhost:${port}`);
-    });
   })
   .catch((err) => {
     console.error(`Data Source initialization error`, err);
   });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "ok" });
+});
+
+app.use("/user", userRouter);
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
