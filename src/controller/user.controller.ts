37a724e3f0712899317ad612770cd76a0aa3ae2b
Ravuthz <ravuthz@gmail.com>
2023-07-05 15:31:42 +0700

    Fixed: Cannot read properties of undefined

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,7 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import UserService from "../services/user.service";
 
 export class UserController {
-  constructor(public userService: UserService) {}
+  constructor(public userService: UserService) {
+    this.get = this.get.bind(this);
+  }
 
   async get(req: Request, res: Response, next: NextFunction) {
     res.json(await this.userService.get());
